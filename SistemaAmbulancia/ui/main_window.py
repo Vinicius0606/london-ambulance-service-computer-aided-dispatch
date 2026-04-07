6abf4dd5491e0f80e35b839e1ed21933ad913346
Rubens de Oliveira Cristiano <rubens.cristiano@a.ucb.br>
2026-04-06 23:26:24 -0300

    feat: Criar a interface da tela principal

---
+++ SistemaAmbulancia/ui/main_window.py
@@ -0,0 +1,349 @@
+import sys
+from PySide6.QtWidgets import (QApplication, QMainWindow, QLabel, QWidget, QHBoxLayout, 
+QFrame, QVBoxLayout, QScrollArea, QSizePolicy, QGraphicsDropShadowEffect, QPushButton)
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtGui import QColor, QPixmap, QIcon
+from datetime import datetime, timedelta
+
+
+class JanelaPrincipal(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        telaUsuarioTamanho = QApplication.instance().primaryScreen().size()
+
+        self.larguraTela = telaUsuarioTamanho.width()
+        self.alturaTela = telaUsuarioTamanho.height()
+
+        self.setWindowTitle("Sistema de Ambulância")
+
+        self.scroll_lista_chamadas = QScrollArea()
+        self.div_lista_chamadas = QFrame()
+        self.layout_lista_chamadas = QVBoxLayout()
+
+        div_geral = QWidget()
+        div_geral.setStyleSheet("background-color: #1d1e27;")
+
+        layout_geral = QHBoxLayout()
+        layout_geral.setContentsMargins(0, 0, 0, 0)
+        layout_geral.setSpacing(0)
+
+        menu_lateral = self.__criar_menu_lateral()
+
+        div_componentes = self.__criar_div_componentes()
+
+        layout_geral.addWidget(menu_lateral, 1)
+        layout_geral.addWidget(div_componentes, 17)
+
+        div_geral.setLayout(layout_geral)
+
+        self.setCentralWidget(div_geral)
+
+    def __criar_menu_lateral(self):
+
+        menu_lateral = QWidget()
+        menu_lateral.backgroundRole()
+        menu_lateral.setStyleSheet("""
+            background-color: #161920;
+        """)
+
+        layout_menu_lateral = QVBoxLayout()
+
+        botao_perfil = QPushButton()
+        botao_perfil.setIcon(QIcon("./assets/user.png"))
+        botao_perfil.setIconSize(QSize(48,48))
+
+        botao_mapa = QPushButton()
+        botao_mapa.setIcon(QIcon("./assets/map.png"))
+        botao_mapa.setIconSize(QSize(48,48))
+
+        botao_sair = QPushButton()
+        botao_sair.setIcon(QIcon("./assets/exit.png"))
+        botao_sair.setIconSize(QSize(48,48))
+
+        layout_menu_lateral.addWidget(botao_perfil, 1, alignment= Qt.AlignTop)
+        layout_menu_lateral.addWidget(botao_mapa, 30, alignment= Qt.AlignTop)
+        layout_menu_lateral.addWidget(botao_sair, 1, alignment= Qt.AlignBottom)
+
+        menu_lateral.setLayout(layout_menu_lateral)
+
+        return menu_lateral
+    
+    def __criar_div_componentes(self):
+       
+        div = QWidget()
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(40,50,0,0)
+
+        texto_chamadas = QLabel("Chamadas Pendentes")
+        texto_chamadas.setAlignment(Qt.AlignLeft)
+        texto_chamadas.setStyleSheet("""
+            color: #dde1f2;
+            font-family: Arial;
+            font-size: 32px;
+            font-weight: bold;
+        """)
+
+        div_chamadas_mapa = self.__criar_div_chamadas_mapa()
+
+        layout.addWidget(texto_chamadas, 1)
+        layout.addWidget(div_chamadas_mapa, 18)
+
+        div.setLayout(layout)
+
+        return div
+
+    def __criar_div_chamadas_mapa(self):
+
+        div = QWidget()
+
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0,0,0,0)
+
+        div_lista_chamadas_pendentes = self.__criar_div_lista_chamadas_pendentes()
+        div_mapa_botao_registrar = self.__criar_div_mapa_botao_registrar()
+
+        layout.addWidget(div_lista_chamadas_pendentes, 1)
+        layout.addWidget(div_mapa_botao_registrar, 1)
+        
+        div.setLayout(layout)
+
+        return div
+    
+    def __criar_div_lista_chamadas_pendentes(self):
+
+        scroll = self.scroll_lista_chamadas
+
+        div = self.div_lista_chamadas
+
+        layout = self.layout_lista_chamadas
+        
+        layout.setContentsMargins(0,10,10,0)
+        layout.setSpacing(20)
+
+        layout.setAlignment(Qt.AlignTop)
+
+        div.setLayout(layout)
+
+        scroll.setWidget(div)
+        scroll.setStyleSheet("""
+            QScrollArea {
+                border: none;
+                background: transparent;
+            }
+
+            QScrollBar:vertical {
+                background: transparent;
+                width: 5px;
+                margin: 0px;
+            }
+
+            QScrollBar::handle:vertical {
+                background: rgba(255, 255, 255, 0.05);
+                border-radius: 2px;
+                min-height: 30px;
+            }
+
+            QScrollBar::handle:vertical:hover {
+                background: rgba(255, 255, 255, 0.1);
+            }
+
+            QScrollBar::add-line:vertical,
+            QScrollBar::sub-line:vertical {
+                height: 0px;
+            }
+
+            QScrollBar::add-page:vertical,
+            QScrollBar::sub-page:vertical {
+                background: transparent;
+            }
+        """)
+
+        scroll.setWidgetResizable(True)
+
+        return scroll
+
+    def adicionarCard(self, nome_paciente: str, prioridade: int, endereco: str, horario_recebido_da_chamada: datetime):
+
+        card = QWidget()
+        card.setFixedHeight(self.scroll_lista_chamadas.height() * 0.25)
+        card.setStyleSheet("""
+            background-color: #20212a;
+            font-family: Arial;
+            font-weight: bold; 
+            border-radius: 20px;      
+        """)
+
+        layout_card = QHBoxLayout()
+        layout_card.setContentsMargins(20,10,20,10)
+
+        shadow = QGraphicsDropShadowEffect()
+
+        shadow.setBlurRadius(50)
+        shadow.setYOffset(8)
+        shadow.setColor(QColor(0,0,0,140))
+
+        card_div_nome_prioridade_endereco = self.__criar_card_div_nome_prioridade_endereco(nome_paciente, prioridade, endereco)
+        card_div_tempo = self.__criar_card_div_tempo(horario_recebido_da_chamada)
+        
+        layout_card.addWidget(card_div_nome_prioridade_endereco)
+        layout_card.addWidget(card_div_tempo)
+
+        card.setLayout(layout_card)
+        card.setGraphicsEffect(shadow)
+
+        self.layout_lista_chamadas.addWidget(card)
+
+    def __criar_card_div_nome_prioridade_endereco(self, nome_paciente: str, prioridade: int, endereco: str):
+
+        prioridade_texto = ""
+        cor_texto = ""
+
+        if prioridade < 5:
+            
+            prioridade_texto = "Baixa Prioridade"
+            cor_texto = "rgba(0, 122, 255"
+
+        elif prioridade < 15: 
+            
+            prioridade_texto = "Media Prioridade"
+            cor_texto = "rgba(255, 193, 7"
+
+        elif prioridade < 25:
+
+            prioridade_texto = "Alta Prioridade"
+            cor_texto = "rgba(255, 140, 0"
+
+        else: 
+
+            prioridade_texto = "Extrema Prioridade"
+            cor_texto = "rgba(220, 53, 69"
+
+        div = QWidget()
+        layout = QVBoxLayout()
+
+        label_nome = QLabel(nome_paciente)
+        label_nome.setStyleSheet("""
+            color: white;
+            font-size: 16px;
+        """)
+
+        label_prioridade = QLabel(prioridade_texto)
+
+        label_prioridade.setStyleSheet(f"""
+            color: {cor_texto + ", 1)"};
+            background-color: {cor_texto + ", 0.25)"};
+            font-size: 14px;
+            border-radius: 10px;
+            padding: 5px 5px;
+        """)
+
+        label_prioridade.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        label_endereco = QLabel(endereco)
+        label_endereco.setStyleSheet("""
+            color: #4f5469;
+            font-size: 14px;
+        """)
+
+        layout.addWidget(label_nome, 1)
+        layout.addWidget(label_prioridade, 1)
+        layout.addWidget(label_endereco, 1)
+
+        div.setLayout(layout)
+
+        return div
+
+    def __criar_card_div_tempo(self, horario_recebido_da_chamada: datetime):
+
+        tempo_atual = datetime.now()
+
+        tempo_total_desde_chamada_recebida = tempo_atual - horario_recebido_da_chamada
+        
+        tempo_texto = ""
+
+        if(tempo_total_desde_chamada_recebida.total_seconds() < 60):
+            
+            tempo_texto = f"{tempo_total_desde_chamada_recebida.total_seconds()} segundo(s) atrás >"
+        
+        else:
+            
+            minutos = (int)(tempo_total_desde_chamada_recebida.total_seconds() / 60)
+
+            tempo_texto = f"{minutos} minuto(s) atrás >"
+
+        div = QWidget()
+        
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignTop)
+        layout.setContentsMargins(0,20,0,0)
+
+        label_tempo = QLabel(tempo_texto)
+        label_tempo.setAlignment(Qt.AlignRight)
+        label_tempo.setStyleSheet("""
+            color: #a1a6b9;
+            font-size: 14px;
+        """)
+
+        layout.addWidget(label_tempo)
+
+        div.setLayout(layout)
+
+        return div
+    
+    def __criar_div_mapa_botao_registrar(self):
+        
+        div = QWidget()
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0,20,0,50)
+        layout.setSpacing(0)
+
+        label_image = QLabel()
+
+        label_image.setFixedWidth(self.larguraTela / 3)
+        label_image.setFixedHeight(self.larguraTela / 4)
+
+        imagem = QPixmap("./assets/mapa.png")
+
+        label_image.setPixmap(imagem)
+        label_image.setScaledContents(True)
+
+        botao = QPushButton("+ Registrar Nova Chamada")
+        botao.setFixedWidth(self.larguraTela / 3)
+
+        botao.setStyleSheet("""
+            background-color: #0f3aaf;
+            padding: 20px 20px;
+            border-radius: 10px;
+            color: white;
+            font-size: 32px;
+            font-family: Arial;
+        """)
+
+        layout.addWidget(label_image, 1, alignment=Qt.AlignTop | Qt.AlignHCenter)
+        layout.addWidget(botao, 1, alignment=Qt.AlignBottom | Qt.AlignHCenter)
+
+        div.setLayout(layout)
+
+        return div
+
+
+app = QApplication(sys.argv)
+
+janela = JanelaPrincipal()
+
+janela.adicionarCard("Teste do Teste", 4, "QN20 Conjunto 10 Casa 28", (datetime.now() - timedelta(hours=3)))
+janela.adicionarCard("Teste", 14, "QN20 Conjunto 1 Casa 8", (datetime.now() - timedelta(hours=2)))
+janela.adicionarCard("Teste", 22, "QN20 Conjunto 1 Casa 8", (datetime.now() - timedelta(hours=0.5)))
+janela.adicionarCard("Teste", 40, "QN20 Conjunto 1 Casa 8", (datetime.now() - timedelta(hours=1)))
+janela.adicionarCard("Teste", 40, "QN20 Conjunto 1 Casa 8", (datetime.now() - timedelta(hours=1)))
+janela.adicionarCard("Teste", 40, "QN20 Conjunto 1 Casa 8", (datetime.now() - timedelta(hours=1)))
+janela.adicionarCard("Teste", 40, "QN20 Conjunto 1 Casa 8", (datetime.now() - timedelta(hours=1)))
+janela.adicionarCard("Teste", 40, "QN20 Conjunto 1 Casa 8", (datetime.now() - timedelta(hours=1)))
+janela.adicionarCard("Teste", 40, "QN20 Conjunto 1 Casa 8", (datetime.now() - timedelta(hours=1)))
+
+janela.showMaximized()
+
+sys.exit(app.exec())
